3b30d49b4d5e40ffdbc5fa0e3460caadbcb0fb27
Devvvina15 <148729784+devinacollege@users.noreply.github.com>
2023-11-18 19:28:12 +0700

    page profil dan edit

--- WEBPBL/EditProfile.php
+++ WEBPBL/EditProfile.php
@@ -47,36 +47,111 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 ?>
 
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Edit Profile</title>
-    <!-- Add CSS or styling as needed -->
+    <style>
+        body {
+            font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+            background: linear-gradient(135deg, #4a90e2, #8e3f95);
+            margin: 0;
+            padding: 0;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 120vh;
+        }
+
+        .edit-profile-container {
+            background-color: #fff;
+            padding: 30px;
+            border-radius: 8px;
+            box-shadow: 0 0 15px rgba(0, 0, 0, 0.1);
+            text-align: center;
+        }
+
+        h1 {
+            color: #333;
+        }
+
+        p {
+            color: #555;
+            margin: 10px 0;
+        }
+
+        form {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+        }
+
+        label {
+            font-weight: bold;
+            margin-top: 10px;
+        }
+
+        input {
+            width: 100%;
+            padding: 10px;
+            margin: 5px 0 20px;
+            box-sizing: border-box;
+            border: 1px solid #ccc;
+            border-radius: 4px;
+            font-size: 16px;
+        }
+
+        button {
+            background-color: #4CAF50;
+            color: #fff;
+            padding: 12px 20px;
+            border: none;
+            border-radius: 4px;
+            cursor: pointer;
+            font-size: 16px;
+            transition: background-color 0.3s;
+        }
+
+        button:hover {
+            background-color: #45a049;
+        }
+    </style>
 </head>
 <body>
-    <h1>Edit Profile</h1>
+    <?php
+    // Your PHP code remains unchanged
+    ?>
 
-    <!-- Display current user information -->
-    <p><strong>Full Name:</strong> <?php echo $userData["full_name"]; ?></p>
-    <p><strong>Email:</strong> <?php echo $userData["email"]; ?></p>
+    <div class="edit-profile-container">
+        <h1>Edit Profil</h1>
 
-    <!-- Edit form -->
-    <form method="post" action="">
-        <label for="new_username">New Username:</label>
-        <input type="text" id="new_username" name="new_username" placeholder="Enter new username">
+        <!-- Display current user information -->
+        <p><strong>Full Name:</strong> <?php echo $userData["full_name"]; ?></p>
+        <p><strong>Email:</strong> <?php echo $userData["email"]; ?></p>
 
-        <label for="new_email">New Email:</label>
-        <input type="email" id="new_email" name="new_email" placeholder="Enter new email">
+        <!-- Edit form -->
+        <form method="post" action="">
+            <label for="new_username">Username Baru:</label>
+            <input type="text" id="new_username" name="new_username" placeholder="Enter new username">
 
-        <label for="new_password">New Password:</label>
-        <input type="password" id="new_password" name="new_password" placeholder="Enter new password">
+            <label for="new_email">Email Baru:</label>
+            <input type="email" id="new_email" name="new_email" placeholder="Enter new email">
 
-        <button type="submit">Save Changes</button>
-    </form>
-    <form action="profile.php" method="get">
-    <button type="submit">Profile</button>
-  </form>
-  <form action="dashboard.php" method="get">
-    <button type="submit">Home</button>
-  </form>
+            <label for="new_password">Password Baru:</label>
+            <input type="password" id="new_password" name="new_password" placeholder="Enter new password">
+
+            <button type="submit">Save Changes</button>
+        </form>
+        <br>
+        <form action="profile.php" method="get">
+            <button type="submit">Profil</button>
+        </form>
+        <br>
+        <form action="dashboard.php" method="get">
+            <button type="submit">Kembali ke Beranda </button>
+        </form>
+        <br>
+    </div>
 </body>
 </html>
--- WEBPBL/profile.php
+++ WEBPBL/profile.php
@@ -30,21 +30,69 @@ if ($userData) {
 ?>
 
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>User Profile</title>
-  <!-- Tambahkan file CSS atau styling sesuai kebutuhan -->
+  <style>
+    body {
+      font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+      background: linear-gradient(135deg, #4a90e2, #8e3f95);
+      margin: 0;
+      padding: 0;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      height: 110vh;
+    }
+
+    .profile-container {
+      background: white;
+      padding: 60px;
+      border-radius: 8px;
+      box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+      text-align: center;
+    }
+
+    h1 {
+      color: #333;
+    }
+
+    p {
+      color: #555;
+      margin: 10px 0;
+    }
+
+    button {
+      background-color: #4CAF50;
+      color: #fff;
+      padding: 10px 20px;
+      border: none;
+      border-radius: 4px;
+      cursor: pointer;
+      font-size: 16px;
+      margin-top: 20px;
+    }
+
+    button:hover {
+      background-color: #45a049;
+    }
+  </style>
 </head>
 <body>
-  <h1>User Profile</h1>
-  <p><strong>Full Name:</strong> <?php echo $fullName; ?></p>
-  <p><strong>Email:</strong> <?php echo $email; ?></p>
-  <p><strong>Password Length:</strong> <?php echo $passwordLength; ?> characters</p>
-  <form action="EditProfile.php" method="get">
-    <button type="submit">Edit Profile</button>
-  </form>
-  <form action="dashboard.php" method="get">
-    <button type="submit">Home</button>
-  </form>
+  <div class="profile-container">
+    <h1>Profil Saya</h1>
+    <img src="ava.jpg" alt="User Avatar" style="width: 200px; height: 200px; border: 1px solid gray;">
+    <p><strong>Full Name:</strong> <?php echo $fullName; ?></p>
+    <p><strong>Email:</strong> <?php echo $email; ?></p>
+    <p><strong>Password Length:</strong> <?php echo $passwordLength; ?> characters</p>
+    <form action="EditProfile.php" method="get">
+      <button type="submit">Edit Profil</button>
+    </form>
+    <form action="dashboard.php" method="get">
+      <button type="submit">Kembali ke Beranda</button>
+    </form>
+  </div>
 </body>
 </html>
